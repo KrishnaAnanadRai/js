8ed2c9579073598e3ee3a0fd44471ca753078582
KrishnaAnandRai <97076031+KrishnaAnanadRai@users.noreply.github.com>
2024-05-23 08:24:51 +0000

    change

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,26 @@
+const accountId = 32313;
+let pass = '3444434';
+var gggg ='ghg';
+city = 'happy';
+let acount;
+
+ console.table([accountId, pass , gggg , city]);
+
+// const value cant be changed
+//  const table is a newfeature in wich we can print  all var at one in tabular format. Syntax console.table([r, t ,tt ,trt])
+// now changing let and var.
+
+pass = '22222';
+gggg = 'yryryry';
+city= 'jaipur';
+
+console.table([accountId, pass , gggg , city, acount]);
+  
+/*
+prefffer not to use var 
+because it not allows to block scope and functional scope.
+*/
+
+// const value cant be changed
+//  const table is a newfeature in wich we can print  all var at one in tabular format. Syntax console.table([r, t ,tt ,trt])
+// now changing let and var.
---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all js code as new version. """NOT NESCESARRY TO USE"""
+// alert("hello ");this can't be used as it is node js not browser.
+
+console.log(3
+    +
+    3
+);// it will work but not at all a good practise make the code readable
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof age); // tells type of string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
