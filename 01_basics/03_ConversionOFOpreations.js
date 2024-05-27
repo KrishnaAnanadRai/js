4683018528451609ff4bb297802d85e99ec7c186
KrishnaAnandRai <97076031+KrishnaAnanadRai@users.noreply.github.com>
2024-05-27 10:41:41 +0000

    c

---
+++ 01_basics/03_ConversionOFOpreations.js
@@ -0,0 +1,9 @@
+let number = 1323;
+let negNumber = -number;
+console.table([number, negNumber]);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);// ** is used to raise the power to 3
+console.log(2/2);
+console.log(2%2);// gives remainder
